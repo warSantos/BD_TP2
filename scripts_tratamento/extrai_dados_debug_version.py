e7eb23903af305bc988a1929bed3c3b1fad2d277
warsantos <weltonarsantos@gmail.com>
2018-11-11 22:50:49 -0200

    Adição. 1. Adição de script de triagem de dados. 2. Adição de scitpt de inserção de dados no mongo. 3. Adição de scripts de paralelização de triagem. 4. Adição de README.txt explicando o processo de triagem e inserção de dados.

--- scripts_tratamento/extrai_dados_debug_version.py
+++ scripts_tratamento/extrai_dados_debug_version.py
@@ -2,6 +2,7 @@
 # argv[2]: Arquivo com os atributos escolhidos.
 # argv[3]: Arquivo com dados para triagem.
 from sys import argv
+import io
 
 def ler_arquivo (path):
 	try:
@@ -13,13 +14,23 @@ def ler_arquivo (path):
 		print 'Erro ao abrir arquivo: ', e
 		exit (1)
 
+def ler_arquivo_utf (path):
+	try:
+		with io.open (path, encoding='utf-8') as content_file:
+			content = content_file.read().split('\n')
+			content.pop()
+			return content
+	except Exception as e:
+		print 'Erro ao abrir arquivo: ', e
+		exit (1)
+
+
 # Cria mascara binaria de atributos utilizados.
 def criar_mascara (atributos, at_utilizados):
 	# Recebendo lista com todos os atributos.
 	lista_atributos = ler_arquivo (atributos)
 	# Recebendo lista com os atributos utilizados.
 	lista_at_ut = ler_arquivo (at_utilizados)
-	tipos = list()
 	mascara = [0] * len (lista_atributos)
 	cont = 0
 	# Criando mascara de bit para identificar quais atributos
@@ -27,50 +38,46 @@ def criar_mascara (atributos, at_utilizados):
 	for a in lista_atributos:
 		if a in lista_at_ut:
 			mascara[cont] = 1
-			tipos.append (a.split()[1])
 		cont += 1
-	return mascara, tipos
+	return mascara
 
 def extrair_dados (mascara, arquivo_dados):
 	dados = ler_arquivo (arquivo_dados)
 	dados_finais = list()
+	utfex = 0
+	linhas_ex = 0
 	atributo_id = [0] * len (mascara)
 	for linha in dados:
 		tokens = linha.split (';')
 		cont = 0
 		temp = list ()
+		excecao = False
 		# Extraindo os valores dos atributos.
 		while cont < len (mascara):
 			# Selecionando os atributos marcados.
 			if (mascara[cont] == 1):
-				att = tokens[cont].replace('\n','').replace ('\r','')
-				temp.append (att)
+				try:
+					att = tokens[cont].replace('\n','').replace ('\r','').encode ('ascii')
+					temp.append (att)#.iencode ('ascii'))
+				except Exception as e:
+					excecao = True
+					utfex += 1
+					atributo_id[cont] += 1
+					#break
 			cont += 1
-		dados_finais.append (temp)
-	return dados_finais
-
-def constroi_json (dados, arquivo_atributos, tipos, prefixo):
-	att_ut = ler_arquivo (arquivo_atributos)
-	ptw_file = open (prefixo+'.json', 'w')
-	for participante in dados:
-		tupla = '{'
-		for att, valor, tipo in zip (att_ut, participante, tipos):
-			# Se for uma string.
-			v_utf = valor.decode ('iso-8859-1').encode ('utf8')
-			if (tipo == '0'):
-				tupla += '"' + att.split ()[0] + '": "' + v_utf + '", '
-			# Se for float ou int.
-			else:
-				tupla += '"' + att.split ()[0] + '": ' + v_utf + ', '
-		tupla = tupla[:-1]
-		tupla = tupla[:-1]
-		tupla += '}\n'
-		ptw_file.write (tupla)
+		# Se nao ocorrerem excecoes.
+		if (not excecao):
+			dados_finais.append (temp)
+		else:
+			linhas_ex += 1
+	return dados_finais, utfex, linhas_ex, atributo_id
 
 if __name__=='__main__':
-
-	mascara, tipos = criar_mascara (argv[1], argv[2])
-	# Recebendo dados triados.
-	dados = extrair_dados (mascara, argv[3])
-	# Criando o arquivo json no formato do mongo.
-	constroi_json (dados, argv[2], tipos, argv[3])
+	dados, utfex, linhas_ex, att = extrair_dados (criar_mascara (argv[1], argv[2]), argv[3])
+	cont = 0
+	print 'Qtde de excecoes: ', utfex
+	print 'Qtde de linhas excedidas: ', linhas_ex
+	j = 0
+	for i in att:
+		print 'Atributo: ', j, ' - ', i
+		j += 1
---
+++ scripts_tratamento/insere_dados.py
@@ -0,0 +1,30 @@
+#argv[1]: arquivos com tuplas no formato JSON.
+from sys import argv, exit
+import pymongo, yaml
+
+def ler_arquivo (path):
+	lista = list()
+	try:
+		with open (path, 'r') as content_file:
+			content = content_file.read().split('\n')
+			content.pop()
+			for t in content:
+				lista.append (yaml.load (t))
+			return lista
+			#return content
+	except Exception as e:
+		print 'Erro ao abrir arquivo: ', e
+		exit (1)
+
+def inserir_dados (arquivos_json):
+	# Carregando JSON
+	tuplas = ler_arquivo (arquivos_json)
+	# Conectando ao MongoDB
+	cliente = pymongo.MongoClient ("mongodb://localhost:27017/")
+	banco = cliente["enem"]
+	# Inserindo dados no banco.
+	colecao = banco["participantes"]
+	retorno = colecao.insert_many (tuplas)
+
+if __name__=='__main__':
+	inserir_dados (argv[1])
